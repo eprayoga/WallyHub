673ee84cfe55267d33f5504d7e14f4c6c26baf35
eprayoga <endangprayoga95@gmail.com>
2023-05-27 21:31:09 +0700

    fix devedencies and add view screen

---
+++ lib/pages/wallpaper_view_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class WallpaperViewPage extends StatefulWidget {
+  const WallpaperViewPage({super.key});
+
+  @override
+  State<WallpaperViewPage> createState() => _WallpaperViewPageState();
+}
+
+class _WallpaperViewPageState extends State<WallpaperViewPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
